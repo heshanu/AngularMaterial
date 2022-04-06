c4a96132ad04536b0a190c4aa0723ef35122596b
heshanu <heshanu97@gmail.com>
2022-04-06 13:56:17 -0700

    add button 4/6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,29 +5,37 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { HomeComponent } from './home/home.component';
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component';
+import { CoursesDialogComponent } from './courses-dialog/courses-dialog.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { ModelComponent } from './model/model.component';
+import { CreateCourseComponent } from './create-course/create-course.component';
+import {MatTabsModule} from '@angular/material/tabs';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HomeComponent, CoursesCardListComponent, CoursesDialogComponent, AboutComponent, CourseComponent, ModelComponent, CreateCourseComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatSliderModule,
     MatSidenavModule,
-    MatCheckboxModule,MatToolbarModule,
+    MatCheckboxModule,
+    MatToolbarModule,
     MatIconModule,
     MatListModule,
-    MatButtonModule
+    MatButtonModule,MatMenuModule,MatTabsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/courses-dialog/courses-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-courses-dialog',
+  templateUrl: './courses-dialog.component.html',
+  styleUrls: ['./courses-dialog.component.css']
+})
+export class CoursesDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
